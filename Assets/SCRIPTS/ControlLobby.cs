cd81c584e4cc0efb7797795cb7505f63649cdfdb
XP_MIGUEL <miguel2001041303@gmail.com>
2024-01-11 11:34:08 -0600

    CONECTARSE AL LOBBY

--- Assets/SCRIPTS/ControlLobby.cs
+++ Assets/SCRIPTS/ControlLobby.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 using Photon.Pun;
 using TMPro;
 using UnityEngine.UI;
+using Photon.Realtime;
 
-public class ControlLobby : MonoBehaviour
+public class ControlLobby : MonoBehaviourPunCallbacks
 {
 
     #region PANEL INICIO
@@ -18,15 +19,101 @@ public class ControlLobby : MonoBehaviour
     private void Awake()
     {
 
+        panelInicio.SetActive(true);
+        panelSeleccion.SetActive(false);
 
+        //Deshabilitar el clic del Boton Inicio 
+        botonIniciar.interactable = false;
 
+        //Borrar la notificacion
+        notificacion.text = string.Empty;
 
     }
 
     private void Start()
     {
 
+        notificacion.text = "Conectandose a Photon";
         PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+    //Este metodo se ejecutara cuando estemos conectados a Photon
+    public override void OnConnectedToMaster()
+    {
+
+        notificacion.text = "Entrando al Lobby ...";
+
+        //Enviarnos al lobby
+        PhotonNetwork.JoinLobby();
+
+    }
+
+    //Este metodo se ejecuta cuando entramos al Lobby
+    public override void OnJoinedLobby()
+    {
+
+        //Esperamos medio segundo a activar el boton de Iniciar
+        Invoke("ActivarBoton", 0.5f);
+
+    }
+
+    private void ActivarBoton()
+    {
+        //Le asignamos funcion al boton
+        botonIniciar.onClick.AddListener(Iniciar);
+
+        //Se puede volver a clicear el boton
+        botonIniciar.interactable = true;
+        notificacion.text = string.Empty;
+        
+    }
+
+    private void Iniciar()
+    {
+
+        //Obtenemos el string del input del Field
+        string nickname = inputNickname.text;
+
+        //Revisar que el string no este vacio
+        if (nickname == string.Empty)
+        {
+            notificacion.text = "! El nickname esta vacio";
+            return;
+        }
+
+        //Revisamos que el nickname no supere los 10 caracteres
+        if (nickname.Length > 10)
+        {
+            notificacion.text = "! El nickname no puede tener mas de 10 caracteres";
+            return;
+        }
+
+        //Asignamos nuestro nickname online
+        PhotonNetwork.NickName = nickname;
+
+        //Si no hay ninguna sala abierta ...
+        if (PhotonNetwork.CountOfRooms == 0)
+        {
+            try
+            {
+                //Tienen que importar Photon.Realtime;
+                var config = new RoomOptions() { MaxPlayers = 8 };
+
+                //Creamos la sala
+                PhotonNetwork.CreateRoom("XP", config);
+            }
+            catch
+            {
+                notificacion.text = "! Problema al crear sala";
+            }
+        }
+
+        //Si ya hay una sala creada
+        else
+        {
+            PhotonNetwork.JoinRoom("XP");
+        }
 
     }
 
@@ -37,7 +124,24 @@ public class ControlLobby : MonoBehaviour
 
     [SerializeField] private GameObject panelSeleccion;
 
+    private void Awake_Seleccion()
+    {
 
+
+
+    }
+
+    //Este metodo se ejecuta cuando entramos a la sala
+    public override void OnJoinedRoom()
+    {
+
+        //Sincronizamos la escena
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+        panelInicio.SetActive(false);
+        panelSeleccion.SetActive(true);
+
+    }
 
     #endregion PANEL SELECCION
 
